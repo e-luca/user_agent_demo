9beec36a3dd5fdd5c94e77bce833566a45fddf3a
e-luca <eldar.lucic@gmail.com>
2024-03-03 12:55:01 +0100

    Initial project setup

---
+++ src/main/java/com/project/useragent/UseragentApplication.java
@@ -0,0 +1,13 @@
+package com.project.useragent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseragentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseragentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/useragent/UseragentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.useragent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseragentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
